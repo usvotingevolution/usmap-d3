41fc377ffed7cc541b99420d694f2cee004fb430
amitkulk123 <amitkulkarni181@gmail.com>
2023-02-28 23:39:09 -0500

    finish fixing the update stuff

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 // Global function called when select element is changed
 function onCategoryChanged() {
-  var select = d3.select("#year").node();
-  var category = select.value;
-  console.log(category);
+  // select the input with type range and id range
+  const selectRange = d3.select("#range").value;
+  console.log(selectRange);
+
+  const selectElement = d3.select("#year").node();
+  const yearFilter = selectElement.value;
+  console.log(yearFilter);
   // Update chart with the selected category of cereal
-  updateChart(category);
+  updateChart(yearFilter);
 }
 
-var data;
+let data;
 
 // Load in my states data!
 d3.csv("stateslived.csv").then(function (dataset) {
@@ -15,88 +19,69 @@ d3.csv("stateslived.csv").then(function (dataset) {
   updateChart("1870");
 });
 
-function updateChart(year_filter) {
-  //Width and height of map
-  var width = 960;
-  var height = 500;
+function updateChart(yearFilter) {
+  const width = 960;
+  const height = 500;
 
-  // D3 Projection
-  var projection = d3
+  const projection = d3
     .geoAlbersUsa()
-    .translate([width / 2, height / 2]) // translate to center of screen
-    .scale([1000]); // scale things down so see entire US
+    .translate([width / 2, height / 2])
+    .scale([1000]);
 
-  // Define path generator
-  var path = d3.geoPath().projection(projection);
+  const pathGenerator = d3.geoPath().projection(projection);
 
-  // Define linear scale for output
-  var color = d3.scaleLinear().range(["rgb(213,222,217)", "rgb(69,173,168)"]);
+  const colorScale = d3
+    .scaleLinear()
+    .range(["rgb(213,222,217)", "rgb(69,173,168)"])
+    .domain([0, 1]);
 
-  var legendText = ["States joined", "Unclaimed"];
+  const svg = d3.select("#map");
 
-  // Select the existing SVG element instead of creating a new one
-  var svg = d3.select("#map");
-
-  // Remove the existing paths and legends
   svg.selectAll("path").remove();
   d3.select(".legend").remove();
 
-  color.domain([0, 1]); // setting the range of the input data
+  const geoJSONFilePath = "us-states.json";
 
-  // Load GeoJSON data and merge with states data
-  d3.json("us-states.json").then(function (json) {
-    // Loop through each state data value in the .csv file
-    for (var i = 0; i < data.length; i++) {
-      // Grab State Name
-      var dataState = data[i].state;
-      // Grab data value
-      var dataValue = data[i].year_admitted;
+  d3.json(geoJSONFilePath).then(function (geoJSON) {
+    for (let i = 0; i < data.length; i++) {
+      const dataState = data[i].state;
+      const dataValue = data[i].year_admitted;
+      for (let j = 0; j < geoJSON.features.length; j++) {
+        const jsonState = geoJSON.features[j].properties.name;
 
-      // Find the corresponding state inside the GeoJSON
-      for (var j = 0; j < json.features.length; j++) {
-        var jsonState = json.features[j].properties.name;
-
-        if (dataState == jsonState) {
-          // Copy the data value into the JSON
-          json.features[j].properties.year_admitted = dataValue;
-          // Stop looking through the JSON
+        if (dataState === jsonState) {
+          geoJSON.features[j].properties.year_admitted = dataValue;
           break;
         }
       }
     }
 
-    // Bind the data to the existing SVG and create one path per GeoJSON feature
     svg
       .selectAll("path")
-      .data(json.features)
+      .data(geoJSON.features)
       .enter()
       .append("path")
-      .attr("d", path)
+      .attr("d", pathGenerator)
       .style("stroke", "#fff")
       .style("stroke-width", "1")
       .style("fill", function (d) {
-        // Get data value
-        // 0 if year_admitted>1870 otherwise 1
-        var value = d.properties.year_admitted > year_filter ? 0 : 1;
+        const value = d.properties.year_admitted > yearFilter ? 0 : 1;
 
         if (value) {
-          //If value exists…
-          return color(value);
+          return colorScale(value);
         } else {
-          //If value is undefined…
           return "rgb(213,222,217)";
         }
       });
 
-    // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
-    var legend = d3
-      .select("body")
+    const legend = d3
+      .select("#map")
       .append("svg")
       .attr("class", "legend")
       .attr("width", 140)
       .attr("height", 200)
       .selectAll("g")
-      .data(color.range().reverse())
+      .data(colorScale.range().reverse())
       .enter()
       .append("g")
       .attr("transform", function (d, i) {
